5ad43fca5e298ea2874510778a1e53c707eee34e
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2023-09-05 17:46:39 +0530

    Time: 12 ms (50.36%), Space: 10.4 MB (65.75%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,40 +1,30 @@
+#include <string>
+#include <vector>
+
 class Solution {
 public:
-    string convert(string s, int r) {
-        int n=s.size();
-        if(r==1)
+    string convert(string s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows) {
             return s;
-
-        int fh=(r-1)*2;
-        int sh=0;
-        int flg=1;
-        string ans;
-        int k=0;
-
-        while(fh>=0)
-        {
-            int temp=k;
-            while(temp<n)
-            {
-                if(flg && fh)
-                {
-                    ans.push_back(s[temp]);
-                    temp+=fh;
-                }
-                else
-                {
-                    if(sh)
-                        ans.push_back(s[temp]);
-                    temp+=sh;
-                }
-
-                flg=1-flg;
-            }
-            flg=1;
-            fh-=2;
-            sh+=2;
-            k++;
         }
-        return ans;
+
+        vector<string> rows(min(numRows, int(s.length())));
+        int currentRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[currentRow] += c;
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        string result;
+        for (string row : rows) {
+            result += row;
+        }
+
+        return result;
     }
 };
